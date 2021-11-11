aae729426a32490606e9b8ffaa8615822be6dfbe
CASSIO645 <cantonio645@gmail.com>
2021-11-11 16:25:52 -0300

    fds

---
+++ myconfig/staticfiles/game/js/scripts.2e1f6a8d06f5.js
@@ -0,0 +1,13 @@
+// pegando a mensagem de feedback e alterando as cores para verde se acertou e vermelho se não acertou
+var txt = document.querySelector("#message");
+var msg = txt.textContent;
+
+    if(msg.includes("Acertou")){
+        txt.style.color = "rgb(0, 107, 0)";
+    } else{
+        txt.style.color = "rgb(138, 15, 15)";
+    }
+
+// Fazendo o input ter o foco após a primeira interação com o jogo
+document.querySelector("#id_resposta").focus();
+
